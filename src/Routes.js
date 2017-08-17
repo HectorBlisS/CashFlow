59b1f32ed2ed97ea3e7def25bf7a5105717a2afe
MiGlez <miglez90290995@gmail.com>
2017-08-17 13:40:09 -0500

    CSS y login page

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import HomePage from './components/HomePage';
-import LoginPage from './components/LoginPage';
+import LoginPage from './components/login/LoginPage';
 
 const Routes = () => (
     <Switch>
-        <Route exact path="/" component={HomePage}/> 
-        <Route path="/login" component={LoginPage} />
+      <Route exact path="/" component={HomePage}/>
+      <Route path="/login" component={LoginPage} />
     </Switch>
-   
+
 );
 
 export default Routes;
