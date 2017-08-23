607b335c43e8765a1d72c6c5922d60b2e971eaf3
Miguel Gonzalez <miglez90290995@gmail.com>
2017-08-23 15:21:28 -0500

    boton log out

--- src/components/login/Perfil.js
+++ src/components/login/Perfil.js
@@ -2,49 +2,60 @@ import React, {Component} from 'react';
 import { Avatar } from 'antd';
 import firebase from '../../firebase';
 import './Login.css';
+import toastr from 'toastr';
 
-class Perfil extends Component{
-    
-    state = {
-        photoURL:'',
-        displayName:'',
-        user:null
-    }
-    
-    componentWillMount(){
-        
-firebase.auth()
-    .onAuthStateChanged(user=> {
-    if(user){
-        this.setState({
-            photoURL:user.photoURL,
-            displayName:user.displayName,
-            user:user
-        });
-    }else{
-        this.props.history.push('/login');
-    }
-});
-    
-    }
-    
-    render(){
-        const {photoURL, displayName,user} = this.state;
-        return(
-            <div>
-               <nav className="nav">
-               <h3>Logo</h3>
-                   {user && <img
-                src={photoURL}
-                />}
-               </nav>
-                
-                <h1>{displayName}</h1>
-            </div>
-            
-            
-        );
-    }
-}
+    class Perfil extends Component{
 
-export default Perfil;
+        state = {
+            photoURL:'',
+            displayName:'',
+            user:null
+        }
+
+        componentWillMount(){
+
+    firebase.auth()
+        .onAuthStateChanged(user=> {
+        if(user){
+            this.setState({
+                photoURL:user.photoURL,
+                displayName:user.displayName,
+                user:user
+            });
+        }else{
+            this.props.history.push('/login');
+        }
+    });
+
+        }
+
+        signOut = () => {
+            firebase.auth().signOut().then(function() {
+                toastr.success("He salido");
+            }, function(error) {
+                toastr.error("No he podido salir");
+            });
+        }
+
+
+        render(){
+            const {photoURL, displayName,user} = this.state;
+            return(
+                <div>
+                   <nav className="nav">
+                       <h3>Logo</h3>
+                           {user && <img
+                        src={photoURL}
+                        />}
+                       </nav>
+
+                        <h1>{displayName}</h1>
+                        <button onClick={this.signOut}>Cerrar Sesión</button>
+                    </div>
+
+
+            );
+        }
+    }
+
+    export default Perfil;
--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,16 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyBzD4uPbVH2TQ52fmYrExTrRjbt7_cb9is",
-  authDomain: "cash-b4513.firebaseapp.com",
-  databaseURL: "https://cash-b4513.firebaseio.com",
-  projectId: "cash-b4513",
-  storageBucket: "cash-b4513.appspot.com",
-  messagingSenderId: "224015766489"
+    apiKey: "AIzaSyBguviCtAxfwAyGqt1nCWjtNr3yL9KvDPc",
+    authDomain: "cashflow-a881b.firebaseapp.com",
+    databaseURL: "https://cashflow-a881b.firebaseio.com",
+    projectId: "cashflow-a881b",
+    storageBucket: "cashflow-a881b.appspot.com",
+    messagingSenderId: "983935095409"
 };
-  firebase.initializeApp(config);
 
-export default firebase; 
+firebase.initializeApp(config);
+
+export default firebase;
 
 
