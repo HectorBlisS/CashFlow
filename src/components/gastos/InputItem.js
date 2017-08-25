5616bcb3f78433f907092fc5a928a56bd946c462
Miguel Gonzalez <miglez90290995@gmail.com>
2017-08-25 08:06:30 -0500

    oh si

--- src/components/gastos/InputItem.js
+++ src/components/gastos/InputItem.js
@@ -2,11 +2,11 @@ import React, {Component} from 'react';
 
 class InputItem extends Component{
     render(){
-        const {tipo,nombre,etiqueta,handleSubmit} = this.props;
+        const {tipo,nombre,etiqueta,handleChange, handleSubmit} = this.props;
         return(
             <div>
                 <label htmlFor={nombre}>{etiqueta}</label>
-                <input type={tipo} name={nombre} id={nombre} onChange={handleSubmit} />
+                <input type={tipo} name={nombre} id={nombre} onChange={handleChange} />
             </div>
         );
     }
