31b4d59f4c98848107cbf11d6038eb56a7fbf947
Miguel Gonzalez <miglez90290995@gmail.com>
2017-09-01 13:45:05 -0500

    sign up validar

--- src/components/signup/SignUpForm.js
+++ src/components/signup/SignUpForm.js
@@ -9,46 +9,69 @@ class SignUpForm extends Component{
           apellidos:'',
             email: '',
             password: ''
-        }
+        },
+        match: true,
+        passwordMatch: ''
     }
 
     handleChange = (e) => {
-        const nombre = e.target.name;
-        const valor = e.target.value;
+        var nombre = e.target.name;
+        var valor = e.target.value;
         let usuario = this.state.usuario;
         usuario[nombre] = valor;
         this.setState({usuario});
     }
 
+    handleChangeMatch = (e) => {
+        const nombre = e.target.name;
+        const valor = e.target.value;
+        this.setState({passwordMatch:valor});
+        if( valor === this.state.usuario.password ){
+            this.setState({match:true});
+        }else{
+            this.setState({match:false});
+        }
+    }
+
     showUserPass = (event) => {
         event.preventDefault();
         toastr.success(this.state.usuario.email);
-        const {signInUserPass} = this.props;
-        const {usuario} = this.state;
-        signInUserPass(usuario.email, usuario.password);
+        if( this.state.passwordMatch === this.state.usuario.password ){
+            const {signInUserPass} = this.props;
+            const {usuario} = this.state;
+            signInUserPass(usuario.email, usuario.password);
+        }else{
+            toastr.error('Las contraseñas no coinciden');
+        }
+
     }
 
     render(){
         const {usuario} = this.state;
+        var color = this.state.match ? 'green' : 'red';
+        var msg = this.state.match ? '' : 'Las contraseñas no coinciden';
         return(
             <div className="center">
                 <form onSubmit={this.showUserPass} className="login-form">
 
                   <label htmlFor="name">Nombre</label>
-                  <input className="textPersonalizado" type="text" id="name " name="names" placeholder="Nombre" onChange={this.handleChange}/>
+                  <input required="required" className="textPersonalizado" type="text" id="name " name="names" placeholder="Nombre" onChange={this.handleChange}/>
 
                   <label htmlFor="apellido">Apellidos</label>
-                  <input className="textPersonalizado" type="text" id="apelli " name="apellido" placeholder="Apellidos" onChange={this.handleChange}/>
+                  <input required="required" className="textPersonalizado" type="text" id="apelli " name="apellido" placeholder="Apellidos" onChange={this.handleChange}/>
 
 
 
                     <label htmlFor="email">Correo</label>
-                    <input value={usuario.correo} className="emailLogin" type="email" id="email" name="email" placeholder="Correo electrónico" onChange={this.handleChange}/>
+                    <input required="required" value={usuario.correo} className="emailLogin" type="email" id="email" name="email" placeholder="Correo electrónico" onChange={this.handleChange}/>
 
-                    <label htmlFor="pass">Contraseña</label>
-                    <input value={usuario.password} type="password" id="pass " name="password" placeholder="Contraseña" onChange={this.handleChange}/>
+                    <label htmlFor="password">Contraseña</label>
+                    <input required="required" value={usuario.password} type="password" id="password" name="password" placeholder="Contraseña" onChange={this.handleChange}/>
 
+                    <label htmlFor="passwordMatch">Contraseña</label>
+                    <input required="required" value={this.state.passwordMatch} type="password" id="passwordMatch" name="passwordMatch" placeholder="Confirmar contraseña" onChange={this.handleChangeMatch}/>
 
+                    <label style={{color:color}}>{msg}</label>
 
                     <input type="submit" value="Registarse" className="aceptarGasto"/>
 
